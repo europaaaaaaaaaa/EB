8f5d89c31415fa212e14f38501dde7b0667f5d73
sanc <pedrodelage435@gmail.com>
2025-12-08 18:51:11 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/8e6724bfc7dcdf2f2b2fc60c5620a8f647b7763267e9b71dac1e35714ea667ab/download"))()
+-- movido para https://gitlab.com/sanctuaryangels/tevez/-/tree/main
